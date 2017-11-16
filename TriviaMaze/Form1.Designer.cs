59035c8664239199a47b459af94b690fdec06b1e
awmccanna <a.mccanna@gmail.com>
2017-11-16 10:07:07 -0800

    First Commit

---
+++ TriviaMaze/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TriviaMaze
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnReset = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1175, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// newGameToolStripMenuItem
+			// 
+			this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+			this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.newGameToolStripMenuItem.Text = "&New Game";
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 27);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(859, 484);
+			this.panel1.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new System.Drawing.Point(877, 27);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(286, 484);
+			this.panel2.TabIndex = 2;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(12, 517);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(98, 37);
+			this.btnStart.TabIndex = 3;
+			this.btnStart.Text = "Start Game";
+			this.btnStart.UseVisualStyleBackColor = true;
+			// 
+			// btnReset
+			// 
+			this.btnReset.Location = new System.Drawing.Point(773, 517);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(98, 37);
+			this.btnReset.TabIndex = 4;
+			this.btnReset.Text = "Reset Game";
+			this.btnReset.UseVisualStyleBackColor = true;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1175, 581);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Main";
+			this.Text = "Trivia Maze";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnReset;
+	}
+}
+
